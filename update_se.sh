50c1afe55602585100b7289f474b4d00ba457896
Ian Gow <iandgow@gmail.com>
2018-09-15 13:19:30 +1000

    Added user messages to update script.

--- update_se.sh
+++ update_se.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 rsync -avz iangow@45.113.235.201:~/uploads/ $SE_DIR
+echo "importing call_files"
 ./create_call_files.R
+echo "importing calls"
 ./import_calls.R
+echo "importing speaker_data"
 ./import_speaker_data.R
