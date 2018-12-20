fb26ad255e43e63ef0f531198880947e581fb0aa
Ian Gow <iandgow@gmail.com>
2018-12-20 10:21:01 -0500

    Updated script to reflect changes in #4.

--- update_se.sh
+++ update_se.sh
@@ -10,6 +10,8 @@ rsync -avz $ID@45.113.235.201:~/uploads/ $SE_DIR
 echo "importing call_files"
 ./create_call_files.R
 echo "importing calls"
-./import_calls.R
+./import_calls_raw.R
+./create_calls.R
+./create_company_ids.R
 echo "importing speaker_data"
 ./import_speaker_data.R
