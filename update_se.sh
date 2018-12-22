b491b25d53d41d5de0fb5dc556927cd6283c9ce0
Ian Gow <iandgow@gmail.com>
2018-12-22 21:06:16 +0000

    Fix script to not load .Rprofile.

--- update_se.sh
+++ update_se.sh
@@ -7,11 +7,13 @@ else
 fi
 
 rsync -avz $ID@45.113.235.201:~/uploads/ $SE_DIR
-echo "importing call_files"
-./create_call_files.R
-echo "importing calls"
-./import_calls_raw.R
-./create_calls.R
-./create_company_ids.R
-echo "importing speaker_data"
-./import_speaker_data.R
+
+echo "Importing call_files"
+echo "$PGHOST"
+Rscript --vanilla create_call_files.R
+echo "Importing calls"
+Rscript --vanilla import_calls_raw.R
+Rscript --vanilla create_calls.R
+Rscript --vanilla create_company_ids.R
+echo "Importing speaker_data"
+Rscript --vanilla import_speaker_data.R
