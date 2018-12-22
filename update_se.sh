8ff86e41592bf5573740d221fcb16e9e32e40a95
Ian Gow <iandgow@gmail.com>
2018-12-22 21:07:09 +0000

    Added create_selected_calls to update script.

--- update_se.sh
+++ update_se.sh
@@ -15,5 +15,6 @@ echo "Importing calls"
 Rscript --vanilla import_calls_raw.R
 Rscript --vanilla create_calls.R
 Rscript --vanilla create_company_ids.R
+Rscript --vanilla create_selected_calls.R
 echo "Importing speaker_data"
 Rscript --vanilla import_speaker_data.R
