e76cbf8fee506728244021732113d5ad99a93d09
Ian Gow <iandgow@gmail.com>
2023-05-16 13:59:41 -0400

    Code from Mac Pro.

--- update_se.sh
+++ update_se.sh
@@ -9,3 +9,19 @@ Rscript --vanilla create_company_ids.R
 Rscript --vanilla create_selected_calls.R
 echo "Importing speaker_data"
 Rscript --vanilla import_speaker_data.R
+if [ "$USER" == "igow" ]
+then
+  ID="iangow"
+else
+  ID=$USER
+fi
+
+rsync -avz $ID@45.113.235.201:~/uploads/ $SE_DIR
+echo "importing call_files"
+Rscript --vanilla create_call_files.R
+echo "importing calls"
+Rscript --vanilla import_calls_raw.R
+Rscript --vanilla create_calls.R
+Rscript --vanilla create_company_ids.R
+echo "importing speaker_data"
+Rscript --vanilla import_speaker_data.R
